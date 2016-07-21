99f72ce7e679e9f126abcb0c4acdeb0c1a9d9fbf
Yongho Kim <ironyonghokim@gmail.com>
2016-07-21 16:11:41 -0500

    use protocol 2 in pickle for backward-compatibility

--- utilities/gPickler.py
+++ utilities/gPickler.py
@@ -13,7 +13,7 @@ def gPickle(data):
 		:param object data: The sensor data to gPickle
 		:rtype: string
 	"""
-	return compress(Pickle.dumps(data))
+	return compress(Pickle.dumps(data, protocol=2))
 
 def un_gPickle(data):
 	"""
